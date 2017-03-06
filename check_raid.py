deac082525938ab160adc115c2685a401213a323
Parth Kolekar <parth.kolekar@students.iiit.ac.in>
2017-03-06 21:04:18 +0530

    backport subprocess.run

--- check_raid.py
+++ check_raid.py
@@ -16,55 +16,22 @@ EXIT_STATUS = {
 }
 
 
-def run_function_wrapper(*popenargs, input=None, timeout=None, check=False, **kwargs):
-    """
-        Shamelessly copied from python3.5 source.
-    """
-    if input is not None:
-        if 'stdin' in kwargs:
-            raise ValueError('stdin and input arguments may not both be used.')
-        kwargs['stdin'] = subprocess.PIPE
-
-    with subprocess.Popen(*popenargs, **kwargs) as process:
-        try:
-            stdout, stderr = process.communicate(input, timeout=timeout)
-        except subprocess.TimeoutExpired:
-            process.kill()
-            stdout, stderr = process.communicate()
-            raise subprocess.TimeoutExpired(
-                process.args,
-                timeout,
-                output=stdout,
-                stderr=stderr
-            )
-        except:
-            process.kill()
-            process.wait()
-            raise
-        retcode = process.poll()
-        if check and retcode:
-            raise subprocess.CalledProcessError(
-                retcode,
-                process.args,
-                output=stdout,
-                stderr=stderr
-            )
-    return subprocess.CompletedProcess(process.args, retcode, stdout, stderr)
-
-
 def check_device(device_node_string):
-    try:
-        completed_process = run_function_wrapper(
+    with subprocess.Popen(
             ("sudo smartctl %s" % (device_node_string)).split(),
-            stdout=subprocess.PIPE
-        )
-        return_code = completed_process.returncode
-    except Exception as e:
-        print(str(e), file=sys.stderr)
-        sys.exit(EXIT_STATUS.get('CRITICAL'))
+            stdout=subprocess.PIPE,
+    ) as running_process:
+        try:
+            output, _ = running_process.communicate()
+            return_code = running_process.poll()
+        except Exception as e:
+            running_process.kill()
+            running_process.wait()
+            print(str(e), file=sys.stderr)
+            sys.exit(EXIT_STATUS.get('CRITICAL'))
 
     if (return_code):
-        print(completed_process.stdout.decode(), file=sys.stderr)
+        print(output, file=sys.stderr)
         if (return_code & 8):
             exit_code = EXIT_STATUS.get('CRITICAL')
             exit_output = "DISK %s: FAILING\n" % (device_node_string)
