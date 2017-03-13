081ee8d404c56a21537f0233e546482a0d6897c6
Parth Kolekar <parth.kolekar@students.iiit.ac.in>
2017-03-14 03:22:57 +0530

    Python2

--- check_raid.py
+++ check_raid.py
@@ -1,11 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import subprocess
 import sys
 
 DEVICES = [
-    '/dev/sda -a',
-    '/dev/sdb -a'
+    '-H /dev/sda',
+    '-H /dev/sdb',
+    '-H /dev/sdc',
+    '-H /dev/sdd'
 ]
 
 EXIT_STATUS = {
@@ -17,21 +19,21 @@ EXIT_STATUS = {
 
 
 def check_device(device_node_string):
-    with subprocess.Popen(
+    running_process = subprocess.Popen(
             ("sudo smartctl %s" % (device_node_string)).split(),
-            stdout=subprocess.PIPE,
-    ) as running_process:
-        try:
-            output, _ = running_process.communicate()
-            return_code = running_process.poll()
-        except Exception as e:
-            running_process.kill()
-            running_process.wait()
-            print(str(e), file=sys.stderr)
-            sys.exit(EXIT_STATUS.get('CRITICAL'))
+            stdout=subprocess.PIPE
+    )
+    try:
+        output, _ = running_process.communicate()
+        return_code = running_process.poll()
+    except Exception, e:
+        running_process.kill()
+        running_process.wait()
+        print(str(e))
+        sys.exit(EXIT_STATUS.get('CRITICAL'))
 
     if (return_code):
-        print(output, file=sys.stderr)
+        print(output)
         if (return_code & 8):
             exit_code = EXIT_STATUS.get('CRITICAL')
             exit_output = "DISK %s: FAILING\n" % (device_node_string)
@@ -51,9 +53,10 @@ def main():
     combined_output = "".join(map(lambda x: x[1], device_status))
     combined_exit_code = tuple(map(lambda x: x[0] != EXIT_STATUS.get('OK'), device_status))
 
-    if any(combined_exit_code):
-        print("CRITICAL\n%s" % (combined_output))
-        sys.exit(EXIT_STATUS.get('CRITICAL'))
+    for i in combined_exit_code:
+        if i:
+            print("CRITICAL\n%s" % (combined_output))
+            sys.exit(EXIT_STATUS.get('CRITICAL'))
     else:
         print("OK\n%s" % (combined_output))
         sys.exit(EXIT_STATUS.get('OK'))
